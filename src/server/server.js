b88e46feb63626d2ecb649b2d9f0b59373e498c2
marq-oh <58898977+marq-oh@users.noreply.github.com>
2020-07-05 08:31:37 -0700

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -17,91 +17,95 @@ const STATUS_CODE_LATE_AIRLINE = 20;
 const STATUS_CODE_LATE_WEATHER = 30;
 const STATUS_CODE_LATE_TECHNICAL = 40;
 const STATUS_CODE_LATE_OTHER = 50;
-const STATUS_CODES  = [
-  STATUS_CODE_UNKNOWN,
-  STATUS_CODE_ON_TIME,
-  STATUS_CODE_LATE_AIRLINE,
-  STATUS_CODE_LATE_WEATHER,
-  STATUS_CODE_LATE_TECHNICAL,
-  STATUS_CODE_LATE_OTHER
-];
-
-// MSJ: For Oracles
-const ORACLES_COUNT = 20;
-let oracles = [];
+const STATUS_CODE_ARR  = [STATUS_CODE_UNKNOWN, STATUS_CODE_ON_TIME, STATUS_CODE_LATE_AIRLINE, STATUS_CODE_LATE_WEATHER, STATUS_CODE_LATE_TECHNICAL, STATUS_CODE_LATE_OTHER];
 
 // MSJ: Get random status code
-function getRandomStatusCode() {
-  return STATUS_CODES[Math.floor(Math.random() * STATUS_CODES.length)];
+function getRandomStatusCode(statusCodeArr) {
+  var currIdx = statusCodeArr.length, temp, index;
+
+  while (currIdx > 0) 
+  {
+      index = Math.floor(Math.random() * currIdx);
+      currIdx--;
+      temp = statusCodeArr[currIdx];
+      statusCodeArr[currIdx] = statusCodeArr[index];
+      statusCodeArr[index] = temp;
+  }
+  return statusCodeArr[0];
+
 }
 
-// MSJ: To register Oracles
+// MSJ: Authorize FlightSuretyApp contract and register oracles
+let oracles = [];
+
 web3.eth.getAccounts().then((accounts) => { 
-  console.log("length :"+ accounts.length);
   flightSuretyData.methods.authorizeCaller(config.appAddress)
      .send({from: accounts[0]})
      .then(result => {
-      console.log("appAddress registered as the authorized contract of dataContract");
+      console.log("FlightSuretyApp authorized");
     })
     .catch(error => {
-      console.log("Error in authorizing appcontract. " + error);
+      console.log(error);
     });
      flightSuretyApp.methods.REGISTRATION_FEE().call().then(fee => {
-      for(let a=1; a<ORACLES_COUNT; a++) {
+      for(let a = 1; a < 20; a++) {
         flightSuretyApp.methods.registerOracle()
         .send({ from: accounts[a], value: fee,gas:4000000 })
         .then(result=>{
           flightSuretyApp.methods.getMyIndexes().call({from: accounts[a]})
           .then(indices =>{
             oracles[accounts[a]] = indices;
-            console.log("Oracle registered: " + accounts[a] + " indices:" + indices);
+            console.log("Oracle: " + accounts[a]);
           })
         }) 
         .catch(error => {
-          console.log("Error while registering oracles: " + accounts[a] +  " Error: " + error);
+          console.log("Error: " + accounts[a] + error);
         });           
       }
      })
 });
 
-// MSJ: For Flight Status Info
-flightSuretyApp.events.FlightStatusInfo({
-  fromBlock: 0
-}, function (error, event) {
-  if (error) console.log(error)
-  else{
-    
-    console.log("Received flightstatusInfo event:  " + JSON.stringify(event));
-    }
-  }
-);
-
-// MSJ: For Oracle request
+// MSJ: Oracle Request Event + SubmitOracleResponse
 flightSuretyApp.events.OracleRequest({fromBlock: 0}, function (error, event) {
   if (error) {
     console.log(error);
   }
   else {
+    let statusCode = getRandomStatusCode();
     let index = event.returnValues.index;
     let airline = event.returnValues.airline;
     let flight = event.returnValues.flight;
     let timestamp = event.returnValues.timestamp;
-    let statusCode = getRandomStatusCode();
 
-    for(let a=0; a<oracles.length; a++) {
+    for(let a = 0; a < oracles.length; a++) {
       if(oracles[a].index.includes(index)) {
         flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, statusCode).send({from: oracles[a].address}, (error, result) => {
           if(error) {
-            console.log(error);
+            console.log("Error: " + oracles[a] + error);
           } 
           else {
-            console.log(`${JSON.stringify(oracles[a])}: Status code ${statusCode}`);
+            console.log("Oracle: " + oracles[a] + statusCode);
           }
         });
       }
     }
   }
 });
+
+// MSJ: Flight Status Info Event
+flightSuretyApp.events.FlightStatusInfo({
+  fromBlock: 0
+}, function (error, event) {
+  if (error) {
+    console.log(error)
+  }
+  else {
+    console.log("FlightstatusInfo:  " + event);
+    }
+  }
+);
+
+
 
 const app = express();
 app.get('/api', (req, res) => {
@@ -111,4 +115,3 @@ app.get('/api', (req, res) => {
 })
 
 export default app;
-
